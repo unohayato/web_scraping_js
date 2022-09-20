e80de13acf7a934355c61b27b57158f23fb44f94
unohayato <mamidori1p1p@gmail.com>
2022-09-20 20:26:52 +0900

    get title

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const PORT = 8000;
 
 const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
+
+//ウェブスクレイパー作成
+const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
+
+axios(URL)
+.then((response) => {
+  const htmlParser = response.data;
+  //console.log(htmlParser);
+
+  const $ = cheerio.load(htmlParser);
+
+  $(".searchresultitem", htmlParser).each(function() {
+    const title = $(this).find(".title").text();
+    console.log(title);
+  });
+});
+
+
+
+
 
 app.listen(PORT, console.log("suver running!"));
